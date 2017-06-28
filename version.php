f7659c62fcecc54a58c6e2a332ee5be91637b715
Matt Davidson <davidso1@rose-hulman.edu>
2017-06-28 14:15:42 -0400

    version.php update

--- version.php
+++ version.php
@@ -25,5 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2017060600;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2017050500;        // Requires this Moodle version.
-$plugin->component = 'tinymce_sketch';       // Full name of the plugin (used for diagnostics).
+$plugin->release   = '1';
+$plugin->requires  = 2015111600;        // Requires this Moodle version.
+$plugin->component = 'tinymce_sketch';
+$plugin->maturity = MATURITY_STABLE;
