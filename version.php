fb7e816b948bf0d337552ba0e929a8053090c43a
Matt Davidson <davidso1@rose-hulman.edu>
2024-01-03 11:07:34 -0500

    Update version.php

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023050500;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = '4.2.0';           // The plugin's latest Moodle version tested with.
+$plugin->version   = 2024010300;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = '4.2.1';           // The plugin's latest Moodle version tested with.
 $plugin->requires  = 2022112800;        // Requires this Moodle version.
 $plugin->component = 'tiny_sketch';
 $plugin->maturity = MATURITY_STABLE;
