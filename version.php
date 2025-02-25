e856940d6b619cc3a27163fe3cacbdda367edad1
Matt Davidson <syxton@gmail.com>
2025-02-25 12:48:46 -0500

    Update version.php

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024091200;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = '4.4.1';           // The plugin's latest Moodle version tested with.
+$plugin->version   = 2025022500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = '4.4.2';           // The plugin's latest Moodle version tested with.
 $plugin->requires  = 2022112800;        // Requires this Moodle version.
 $plugin->component = 'tiny_sketch';
 $plugin->maturity = MATURITY_STABLE;
